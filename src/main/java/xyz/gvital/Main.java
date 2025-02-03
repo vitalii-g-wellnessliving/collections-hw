c954ebd4ff6fa8b2cfa53df3efb6be967955b738
Vitalii Goncharov <1c.audit@gmail.com>
2025-02-03 22:49:15 +0200

    add checkstyle

--- src/main/java/xyz/gvital/Main.java
+++ src/main/java/xyz/gvital/Main.java
@@ -1,6 +1,10 @@
 package xyz.gvital;
 
-public class Main {
+final class Main {
+    private Main() {
+        throw new IllegalStateException("Utility class");
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
