8fc0db07be38a024d495c446043a7ce8d7980782
Vitalii Goncharov <1c.audit@gmail.com>
2025-02-05 09:53:46 +0200

    reverse linked list solution

--- src/main/java/xyz/gvital/Main.java
+++ src/main/java/xyz/gvital/Main.java
@@ -6,6 +6,27 @@ final class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // 1
+        @SuppressWarnings("magicnumber")
+        ListNode head = new ListNode(new int[]{1, 2, 3, 4, 5});
+        System.out.println(reverseList(head));
+
+        // 2
+        String input = "([]{}[])";
+        System.out.println(isClosed(input));
+    }
+
+    // Given the head of a singly linked list, reverse the list, and return the reversed list.
+    // https://leetcode.com/problems/reverse-linked-list/
+    static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
     }
 }
